347a5e4d232971f5e214ad1a951045ee24d7bd6f
bluedr <skordos88@gmail.com>
2024-05-17 13:50:32 +0300

    breake the coding brefore end the gas db import data

--- app/Http/Controllers/ManageFuel.php
+++ app/Http/Controllers/ManageFuel.php
@@ -6,16 +6,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use App\Models\Vehicle;
 use App\models\Gas_type;
+use App\models\Gas;
 use Auth;
 
 class ManageFuel extends Controller
 {
-    public function fuel_consumption($id) {        
+    public function fuel_consumption($id, Request $request) {        
         $Uid = Auth::id();
         $gas_t = Gas_type::all();
         $vehicles = Vehicle::with('user','gas_type')->where('user_id',$Uid)->find($id);
         if($vehicles == null)
             return Redirect::route('index')->withErrors('Error, no data to illustrate.');
+        if($request->isMethod('POST')) { //TODO: na ftiakso edo tin kataxorisi toy gas
+            $gas = new Gas();
+            $gas->vehicle_id = $vehicles->id;
+            $gas->km = 11112;
+            $gas->lt = 1112;
+            $gas->save();
+        }
         return view('fuel-consumption',compact('vehicles'));
     }
 }
